7cb960c384fd234c24d5ca8912106b8f2e991ebb
Eric O <eric@ejdigitalmedia.com>
2022-01-31 16:45:34 -0500

    Update prod deploy config to temporarily point to ldpd-wp-test1

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,4 +1,4 @@
-server "#{fetch(:department)}-nginx-#{fetch(:stage)}1.cul.columbia.edu", user: fetch(:remote_user), roles: %w{web app}
+server "ldpd-wp-test1.cul.columbia.edu", user: fetch(:remote_user), roles: %w{web app}
 ask :branch, proc { `git tag --sort=version:refname`.split("\n").last }
 
 set :url, 'https://socialtextjournal.org'
