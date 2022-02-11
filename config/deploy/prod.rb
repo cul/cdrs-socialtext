3cf42f1dbaa3c5b55e53fbca45e540db97682fcb
Eric O <eric@ejdigitalmedia.com>
2022-02-11 13:27:41 -0500

    Updated prod deploy target

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,4 +1,4 @@
-server "ldpd-wp-test1.cul.columbia.edu", user: fetch(:remote_user), roles: %w{web app}
+server "ldpd-wp-prod1.cul.columbia.edu", user: fetch(:remote_user), roles: %w{web app}
 ask :branch, proc { `git tag --sort=version:refname`.split("\n").last }
 
 set :url, 'https://socialtextjournal.org'
