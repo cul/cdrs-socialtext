a14ed16b001638174716bac6a53362e0d44451ce
Eric O <eric@ejdigitalmedia.com>
2024-11-01 13:49:09 -0400

    Update to capistrano-cul 0.3.3 and update deploy.rb and deployment targets for new AlmaLinux server deployment

--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -1,4 +1,4 @@
-server "ldpd-wp-test1.cul.columbia.edu", user: fetch(:remote_user), roles: %w{web app}
+server "ds-wp-test1.cul.columbia.edu", user: fetch(:remote_user), roles: %w{web app}
 ask :branch, proc { `git tag --sort=version:refname`.split("\n").last }
 
 set :url, 'https://socialtext-test.cul.columbia.edu'
